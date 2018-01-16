ad37bd1a3eecb6256d77637c2dba523772b013f2
Nicolas Chapon <nicolas.chapon@cnp.fr>
2018-01-16 18:08:26 +0100

    Add Kong OpenID connect config

---
+++ docker/kong-oidc/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+# Disabling nginx daemon mode
+export KONG_NGINX_DAEMON="off"
+
+# Export Kong OIDC
+export KONG_CUSTOM_PLUGINS=oidc
+
+# Setting default prefix (override any existing variable)
+export KONG_PREFIX="/usr/local/kong"
+
+# Prepare Kong prefix
+if [ "$1" = "/usr/local/openresty/nginx/sbin/nginx" ]; then
+    kong prepare -p "/usr/local/kong"
+fi
+
+exec "$@"
